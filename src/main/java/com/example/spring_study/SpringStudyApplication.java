1987adedee3c36ebccde7708d3353e18a9b60d16
sunkeling <454541398@qq.com>
2024-07-01 14:54:14 +0800

    test

--- src/main/java/com/example/spring_study/SpringStudyApplication.java
+++ src/main/java/com/example/spring_study/SpringStudyApplication.java
@@ -7,7 +7,10 @@ public class SpringStudyApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(SpringStudyApplication.class, args);
-        System.out.println("Hello World");
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+
+
     }
 
 }
